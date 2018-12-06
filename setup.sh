b39c8b5699f056cf9abd28ee8dfaa25c431375a7
Jun Ito <jun_ito@voyagegroup.com>
2018-12-06 19:26:33 +0900

    Update: setupを整理

--- setup.sh
+++ setup.sh
@@ -42,43 +42,44 @@ brew_install_if_not_exists pyenv
 brew_install_if_not_exists rbenv
 brew_install_if_not_exists nodenv
 brew_install_if_not_exists goenv
+brew_install_if_not_exists pipenv
 
-cask_install_if_not_exists sketch
-cask_install_if_not_exists clipy
-cask_install_if_not_exists franz
-cask_install_if_not_exists dropbox
-cask_install_if_not_exists karabiner-elements
-cask_install_if_not_exists alfred
-cask_install_if_not_exists bettertouchtool
+
 cask_install_if_not_exists sublime-text
 cask_install_if_not_exists iterm2
 cask_install_if_not_exists google-chrome
 cask_install_if_not_exists google-japanese-ime
+cask_install_if_not_exists microsoft-office
+
 cask_install_if_not_exists unity
+cask_install_if_not_exists sketch
+cask_install_if_not_exists franz
+cask_install_if_not_exists dropbox
+
+cask_install_if_not_exists clipy
+cask_install_if_not_exists karabiner-elements
+cask_install_if_not_exists alfred
+cask_install_if_not_exists bettertouchtool
 
 mas install 539883307 # LINE
 mas install 409183694 # Keynote
 
-# Automatically hide or show the Dock （Dock を自動的に隠す）
 defaults write com.apple.dock autohide -bool false
-# Wipe all app icons from the Dock （Dock に標準で入っている全てのアプリを消す、Finder とごみ箱は消えない）
 defaults write com.apple.dock persistent-apps -array
-# Set the icon size （アイコンサイズの設定）
 defaults write com.apple.dock tilesize -int 50
-# Magnificate the Dock （Dock の拡大機能を入にする）
 defaults write com.apple.dock magnification -bool false
 
 defaults write -g com.apple.trackpad.scaling 6
 defaults write -g KeyRepeat -int 1
 defaults write -g InitialKeyRepeat -int 5
 
-mkdir -p ~/.config/karabiner
-
-cp $(PWD)/dotfiles/.gitconfig ~/
+cp -r $(PWD)/dotfiles/.vim ~/
 cp $(PWD)/dotfiles/.vimrc ~/
 cp $(PWD)/dotfiles/.zshrc ~/
-cp -r $(PWD)/dotfiles/.vim ~/
+cp $(PWD)/dotfiles/.gitconfig ~/
 
+mkdir -p ~/.config/karabiner
 cp $(PWD)/karabiner.json ~/.config/karabiner/karabiner.json
+
 echo zsh > ~/.bash_profile
 zsh
