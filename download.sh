85496e0eb7c1f888239d4a61250832ab4c8ebaf0
伊藤淳 <j-ito@macintosh.lan>
2018-12-06 10:39:37 +0900

    brewのところまで

--- download.sh
+++ download.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
-
 function command_exists {
   command -v "$1" > /dev/null;
+}
+
+function brew_install_if_not_exist {
+if ! command_exists $1 ; then
+  echo " ------------ $1 ------------"
+  brew install $1
+  $1 --version
+  echo " ------------ END ------------"
+else 
+  echo "$1 already installed."
+fi
 }
 
 if ! command_exists brew ; then
@@ -13,19 +22,38 @@ if ! command_exists brew ; then
   echo " ------------ END ------------"
 fi
 
-if ! command_exists git ; then
-  echo " ------------ Git ------------"
-  brew install git
-  git --version
-  echo " ------------ END ------------"
-fi
+brew_install_if_not_exist git
+brew_install_if_not_exist mas
 
-if ! command_exists mas ; then
-  echo " ------------ Mas ------------"
-  brew install mas
-  echo " ------------ END ------------"
-fi
+brew_install_if_not_exist pyenv
+brew_install_if_not_exist rbenv
+brew_install_if_not_exist nodenv
+brew_install_if_not_exist goenv
 
-brew cask install iterm2
-brew cask install google-chrome
 
+
+# brew cask install Karabiner-Elements
+# brew cask install alfred
+# brew cask install bettertouchtooL
+
+# brew cask install sublime-text
+
+#brew cask install iterm2
+#brew cask install google-chrome
+# Automatically hide or show
+
+# the Dock （Dock を自動的に隠す）
+defaults write com.apple.dock autohide -bool true
+# Wipe all app icons from the Dock （Dock に標準で入っている全てのアプリを消す、Finder とごみ箱は消えない）
+defaults write com.apple.dock persistent-apps -array
+# Set the icon size （アイコンサイズの設定）
+defaults write com.apple.dock tilesize -int 30
+# Magnificate the Dock （Dock の拡大機能を入にする）
+defaults write com.apple.dock magnification -bool false
+
+#cp $(PWD)/.zshrc ~/.zshrc
+mkdir -p ~/.config
+cp $(PWD)/karabiner.json ~/.config/karabiner/karabiner.json
+zsh > bash-profile
+zsh
+-
