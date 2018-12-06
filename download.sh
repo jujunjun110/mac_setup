1bfa36f1bf6fa64025d5ba332ac740b2453f301d
伊藤淳 <j-ito@macintosh.lan>
2018-12-06 10:48:59 +0900

    caskも関数化

--- download.sh
+++ download.sh
@@ -2,11 +2,26 @@ function command_exists {
   command -v "$1" > /dev/null;
 }
 
+function cask_installed {
+  brew cask list $1 > /dev/null;
+}
+
 function brew_install_if_not_exist {
 if ! command_exists $1 ; then
   echo " ------------ $1 ------------"
   brew install $1
   $1 --version
+  echo " ------------ END ------------"
+else 
+  echo "$1 already installed."
+fi
+}
+
+function cask_install_if_not_exist {
+if ! cask_installed $1 ; then
+  echo " ------------ $1 ------------"
+  brew cask install $1
+  # $1 --version
   echo " ------------ END ------------"
 else 
   echo "$1 already installed."
@@ -32,17 +47,14 @@ brew_install_if_not_exist goenv
 
 
 
-# brew cask install Karabiner-Elements
-# brew cask install alfred
-# brew cask install bettertouchtooL
+cask_install_if_not_exist Karabiner-Elements
+cask_install_if_not_exist alfred
+cask_install_if_not_exist bettertouchtooL
+cask_install_if_not_exist sublime-text
+cask_install_if_not_exist iterm2
+cask_install_if_not_exist google-chrome
 
-# brew cask install sublime-text
-
-#brew cask install iterm2
-#brew cask install google-chrome
-# Automatically hide or show
-
-# the Dock （Dock を自動的に隠す）
+# Automatically hide or show the Dock （Dock を自動的に隠す）
 defaults write com.apple.dock autohide -bool true
 # Wipe all app icons from the Dock （Dock に標準で入っている全てのアプリを消す、Finder とごみ箱は消えない）
 defaults write com.apple.dock persistent-apps -array
@@ -51,9 +63,8 @@ defaults write com.apple.dock tilesize -int 30
 # Magnificate the Dock （Dock の拡大機能を入にする）
 defaults write com.apple.dock magnification -bool false
 
-#cp $(PWD)/.zshrc ~/.zshrc
+cp $(PWD)/.zshrc ~/.zshrc
 mkdir -p ~/.config
 cp $(PWD)/karabiner.json ~/.config/karabiner/karabiner.json
-zsh > bash-profile
+# zsh > bash-profile
 zsh
--
