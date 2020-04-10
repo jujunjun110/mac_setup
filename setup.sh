21eeee1686f5102481ff89ff66f2ed30528700f8
Jun Ito <jun_ito@voyagegroup.com>
2020-04-10 16:05:54 +0900

    Update: install order

--- setup.sh
+++ setup.sh
@@ -39,67 +39,6 @@ if ! command_exists brew ; then
   echo " ------------ END ------------"
 fi
 
-brew_install_if_not_installed git
-brew_install_if_not_installed mas
-brew_install_if_not_installed zsh
-
-brew_install_if_not_installed pyenv
-brew_install_if_not_installed rbenv
-brew_install_if_not_installed nodenv
-brew_install_if_not_installed goenv
-brew_install_if_not_installed pipenv
-
-brew_install_if_not_installed tree
-brew_install_if_not_installed colordiff
-brew_install_if_not_installed rails
-brew_install_if_not_installed gibo
-brew_install_if_not_installed mono
-brew_install_if_not_installed openssl
-brew_install_if_not_installed mysql
-brew_install_if_not_installed hub
-
-cask_install_if_not_installed google-chrome
-cask_install_if_not_installed firefox
-cask_install_if_not_installed google-japanese-ime
-cask_install_if_not_installed franz
-
-cask_install_if_not_installed sublime-text
-cask_install_if_not_installed iterm2
-cask_install_if_not_installed unity-hub
-cask_install_if_not_installed sketch
-cask_install_if_not_installed blender
-cask_install_if_not_installed dropbox
-cask_install_if_not_installed openoffice
-cask_install_if_not_installed docker
-cask_install_if_not_installed microsoft-office
-
-cask_install_if_not_installed google-backup-and-sync
-cask_install_if_not_installed clipy
-cask_install_if_not_installed keycastr
-cask_install_if_not_installed karabiner-elements
-cask_install_if_not_installed alfred
-cask_install_if_not_installed bettertouchtool
-cask_install_if_not_installed visual-studio-code
-cask_install_if_not_installed dotnet
-
-mas install 497799835 # Xcode
-mas install 539883307 # LINE
-mas install 409183694 # Keynote
-mas install 668208984 # GIPHY
-mas install 915542151 # Monity
- 
-if !(which python | grep -sq shims); then
-  pyenv install $(pyenv install -l | grep -v - | tail -1)
-  pyenv global $(pyenv install -l | grep -v - | tail -1)
-  pyenv rehash
-fi
-
-if !(which ruby | grep -sq shims); then
-  rbenv install $(rbenv install -l | grep -v - | tail -1)
-  rbenv global $(rbenv install -l | grep -v - | tail -1)
-  rbenv rehash
-fi
-
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 defaults write com.apple.dock autohide -bool false
 defaults write com.apple.dock persistent-apps -array
@@ -121,3 +60,63 @@ cp $(PWD)/karabiner.json ~/.config/karabiner/karabiner.json
 
 echo zsh > ~/.bash_profile
 zsh
+
+brew_install_if_not_installed git
+brew_install_if_not_installed mas
+brew_install_if_not_installed zsh
+cask_install_if_not_installed iterm2
+
+cask_install_if_not_installed keycastr
+cask_install_if_not_installed karabiner-elements
+cask_install_if_not_installed alfred
+cask_install_if_not_installed bettertouchtool
+cask_install_if_not_installed clipy
+
+cask_install_if_not_installed sublime-text
+cask_install_if_not_installed google-chrome
+cask_install_if_not_installed firefox
+cask_install_if_not_installed google-japanese-ime
+cask_install_if_not_installed franz
+cask_install_if_not_installed microsoft-office
+
+brew_install_if_not_installed tree
+brew_install_if_not_installed colordiff
+brew_install_if_not_installed rails
+brew_install_if_not_installed gibo
+brew_install_if_not_installed mono
+brew_install_if_not_installed openssl
+brew_install_if_not_installed mysql
+brew_install_if_not_installed hub
+
+cask_install_if_not_installed dropbox
+cask_install_if_not_installed google-backup-and-sync
+cask_install_if_not_installed visual-studio-code
+
+cask_install_if_not_installed dotnet
+brew_install_if_not_installed pyenv
+brew_install_if_not_installed rbenv
+brew_install_if_not_installed nodenv
+brew_install_if_not_installed goenv
+brew_install_if_not_installed pipenv
+cask_install_if_not_installed blender
+cask_install_if_not_installed unity-hub
+cask_install_if_not_installed sketch
+cask_install_if_not_installed docker
+
+mas install 497799835 # Xcode
+mas install 539883307 # LINE
+mas install 409183694 # Keynote
+mas install 668208984 # GIPHY
+mas install 915542151 # Monity
+ 
+if !(which python | grep -sq shims); then
+  pyenv install $(pyenv install -l | grep -v - | tail -1)
+  pyenv global $(pyenv install -l | grep -v - | tail -1)
+  pyenv rehash
+fi
+
+if !(which ruby | grep -sq shims); then
+  rbenv install $(rbenv install -l | grep -v - | tail -1)
+  rbenv global $(rbenv install -l | grep -v - | tail -1)
+  rbenv rehash
+fi
