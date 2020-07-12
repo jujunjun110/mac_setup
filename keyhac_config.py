5d34f0ae980d674fd5d7a76414c81d7e3b5ca574
Jun Ito <jun_ito@voyagegroup.com>
2020-07-13 00:33:20 +0900

    keyhac mouse movement

--- keyhac_config.py
+++ keyhac_config.py
@@ -34,3 +34,38 @@ def configure(keymap):
 
     keymap_global["RC-I"] = "Enter"
     keymap_global["RC-O"] = "End", "Enter"
+    
+
+    # 単純なキー入れ替え
+    keymap.replaceKey( "29", 235 ) # 無変換キーに仮想コード235を割り当て
+    keymap.replaceKey( "28", 236 ) # 変換キーに仮想コード236を割り当て
+    
+    # ユーザモディファイアの設定
+    keymap.defineModifier( 235, "User0" )
+    keymap.defineModifier( 236, "User1" )
+
+    def ime_on():
+        keymap.getWindow().setImeStatus(1)
+
+    def ime_off():
+        keymap.getWindow().setImeStatus(0)
+
+    keymap_global[ "U0-I" ] = "LButton"
+    keymap_global[ "U1-I" ] = "LButton"
+    keymap_global[ "U0-O" ] = "RButton"
+    keymap_global[ "U1-O" ] = "RButton"
+
+    keymap_global[ "U0-J" ] = keymap.MouseMoveCommand(0, 20)
+    keymap_global[ "U0-K" ] = keymap.MouseMoveCommand(0, -20)
+    keymap_global[ "U0-L" ] = keymap.MouseMoveCommand(20, 0)
+    keymap_global[ "U0-H" ] = keymap.MouseMoveCommand(-20, 0)
+    keymap_global[ "U0-B" ] = keymap.MouseMoveCommand(-20, 0)
+
+    keymap_global[ "U1-J" ] = keymap.MouseMoveCommand(0, 10)
+    keymap_global[ "U1-K" ] = keymap.MouseMoveCommand(0, -10)
+    keymap_global[ "U1-L" ] = keymap.MouseMoveCommand(10, 0)
+    keymap_global[ "U1-H" ] = keymap.MouseMoveCommand(-10, 0)
+    keymap_global[ "U1-B" ] = keymap.MouseMoveCommand(-10, 0)
+
+    keymap_global[ "O-235" ] = ime_off
+    keymap_global[ "O-236" ] = ime_on
