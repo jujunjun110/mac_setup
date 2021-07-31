f2d4b9ef5bfdbacfc73a7f15dabb176b4e7c4b6c
Jun Ito <jujunjun110@gmail.com>
2021-07-31 14:39:04 +0900

    Update: brew bundle化に際して不要な関数を削除

--- setup.sh
+++ setup.sh
@@ -2,38 +2,10 @@ function command_exists {
   command -v "$1" > /dev/null;
 }
 
-function cask_installed {
-  # ls /usr/local/Caskroom/ | grep $1 > /dev/null;
-  ls /usr/local/Caskroom/ | grep -sq $1;
-}
-
-function brew_install_if_not_installed {
-  if command_exists $1 ; then
-    echo "$1 already installed."
-    return 
-  fi
-
-  echo " ------------ $1 ------------"
-  brew install $1
-  $1 --version
-  echo " ------------ END ------------"
-}
-
-function cask_install_if_not_installed {
-  if cask_installed $1 ; then
-    echo "$1 already installed."
-    return
-  fi
-
-  echo " ------------ $1 ------------"
-  brew install cask $1
-  echo " ------------ END ------------"
-}
-
 if ! command_exists brew ; then
   echo " --------- Homebrew ----------"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/j-ito/.zprofile
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
   brew update
   brew upgrade --all --cleanup
@@ -49,7 +21,6 @@ defaults write com.apple.dock tilesize -int 50
 defaults write com.apple.dock magnification -bool false
 defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock orientation -string "left"
-
 
 defaults write -g com.apple.trackpad.scaling 6
 defaults write -g KeyRepeat -int 2
