b92287634675ccd29818f762709467e781d207f1
Jun Ito <jujunjun110@gmail.com>
2021-07-31 15:34:10 +0900

    Update: misc

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 function command_exists {
-  command -v "$1" > /dev/null;
+    command -v "$1" > /dev/null;
 }
 
 if ! command_exists brew ; then
-  echo " --------- Homebrew ----------"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-  brew update
-  brew upgrade --all --cleanup
-  brew -v
-  echo " ------------ END ------------"
+    echo " --------- Homebrew ----------"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    brew update
+    brew upgrade --all --cleanup
+    brew -v
+    echo " ------------ END ------------"
 fi
 
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
@@ -25,6 +25,11 @@ defaults write com.apple.dock orientation -string "left"
 defaults write -g com.apple.trackpad.scaling 6
 defaults write -g KeyRepeat -int 2
 defaults write -g InitialKeyRepeat -int 30
+
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
+
+echo .DS_Store > ~/.gitignore
 
 cp -r $(PWD)/dotfiles/.vim ~/
 cp $(PWD)/dotfiles/.vimrc ~/
